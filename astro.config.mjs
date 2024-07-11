a85c678a0a0e7af94367482c440ad52eae6f7199
tdimnet <thomas.dimnet@gmail.com>
2024-07-11 19:58:48 +0200

    Updating astro config to match repo path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://tdimnet.github.io',
+    base: 'Frontend-Quiz-app'
+});
